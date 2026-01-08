30facb28db8858594db8535d71cf7de9400c4641
Michal Blaha <michal@michalblaha.cz>
2026-01-08 23:03:32 +0100

    performance improvements and local settings

--- Program.cs
+++ Program.cs
@@ -20,16 +20,28 @@ namespace CountLines
             long totalSize = new System.IO.FileInfo(path).Length;
 
             int lines = 0;
+            byte[] buffer = new byte[1024 * 1024]; // 1MB buffer
+            int bytesRead;
+            long totalBytesRead = 0;
+            long nextReportAt = totalSize / 20; // Report every 5%
+
             using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (BufferedStream bs = new BufferedStream(fs))
-            using (StreamReader sr = new StreamReader(bs))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    lines++;
-                    if (lines % 50 == 0)
-                        Console.WriteLine($"{((double)bs.Position / (double)totalSize).ToString("P2")} ({lines})");
+                    for (int i = 0; i < bytesRead; i++)
+                    {
+                        if (buffer[i] == '\n') lines++;
+                    }
+
+                    totalBytesRead += bytesRead;
+
+                    // Report progress every 5%
+                    if (totalBytesRead >= nextReportAt)
+                    {
+                        Console.WriteLine($"{((double)totalBytesRead / (double)totalSize).ToString("P2")} ({lines} lines)");
+                        nextReportAt += totalSize / 20;
+                    }
                 }
             }
 
